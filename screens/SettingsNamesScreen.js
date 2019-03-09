75e1d8d58d09906bad303a042209cd853d8e2adc
Tristan Perera <tristan.e.perera@gmail.com>
2019-03-08 16:43:21 -0800

    added styling to names settings

--- screens/SettingsNamesScreen.js
+++ screens/SettingsNamesScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, TouchableOpacity, View } from 'react-native'
 import { Container, Content, Form, Item, Input, Button, Picker, Label } from 'native-base';
+import { Grid, Row } from 'react-native-easy-grid'
 
 import Nextbutton from '../components/NextButton'
 
@@ -44,11 +45,13 @@ export default class SettingsScreen extends Component {
       <Container>
         <AppConsumer>
           {(context) => (
-            <Content>
-              <Form style={styles.form}>
-
-                <Item floatingLabel>
-                <Label>PLAYER ONE</Label>
+            <Grid>
+              <Row size={60} style={styles.form}>
+                <Item
+                  style={styles.formItem}
+                  floatingLabel
+                >
+                  <Label>PLAYER ONE</Label>
                   <Input
                     value={this.state.playerOne.name}
                     onChangeText={(text) => {
@@ -59,8 +62,11 @@ export default class SettingsScreen extends Component {
                   />
                 </Item>
 
-                <Item floatingLabel>
-                <Label>PLAYER TWO</Label>
+                <Item
+                  style={styles.formItem}
+                  floatingLabel
+                >
+                  <Label>PLAYER TWO</Label>
                   <Input
                     value={this.state.playerTwo.name}
                     onChangeText={(text) => {
@@ -70,13 +76,17 @@ export default class SettingsScreen extends Component {
                     }}
                   />
                 </Item>
+              </Row>
 
-                  <Nextbutton 
-                  style={{alignSelf: "flex-end"}}
-                  path={"SettingsSecondaries"} />
+              <Row size={7} style={styles.nextButton}>
+                <View>
+                  <Nextbutton
+                    path={"SettingsSecondaries"} />
+                </View>
+              </Row>
 
-              </Form>
-            </Content>
+
+            </Grid>
           )}
         </AppConsumer>
       </Container>
@@ -90,10 +100,21 @@ const styles = StyleSheet.create({
   },
   form: {
     // backgroundColor: "red",
-    display: "flex",
     flexDirection: "column",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-  }
+    // flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  formItem: {
+    padding: 15,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  nextButton: {
+    display: "flex",
+    // flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    // alignItems: "stretch",
+  },
 });
 
